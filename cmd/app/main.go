e5c04e6498b0eb7473cab983f4fc977550ea7a4d
kirei-io <44925378+kirei-io@users.noreply.github.com>
2025-12-30 10:15:20 +0300

    feat: web app docker

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kirei-io/go-auth-playground/internal/auth"
@@ -14,7 +15,16 @@ func main() {
 	cfg := core.GetConfig()
 	database.InitDb(&cfg.Db)
 
-	r := gin.Default()
+	r := gin.New()
+
+	r.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+		SkipPaths: []string{"/health"},
+	}))
+	r.Use(gin.Recovery())
+
+	r.GET("/health", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
 
 	authRepo := auth.NewAuthRepository(database.Client)
 	authService := auth.NewAuthService(&cfg.Auth, authRepo)
